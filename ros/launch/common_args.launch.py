f4d33c5b3d817f7a006c8ca0bd175dcb10656845
michel.wagemann <michel.wagemann@uni-bielefeld.de>
2025-07-22 15:37:18 +0200

    add parameter

--- ros/launch/common_args.launch.py
+++ ros/launch/common_args.launch.py
@@ -7,6 +7,7 @@ def generate_launch_description():
         [
             DeclareLaunchArgument(
                 "lidar_topic",
+                default_value="/cloud",
                 description="",
             ),
             DeclareLaunchArgument(
@@ -20,24 +21,25 @@ def generate_launch_description():
                 default_value="lidar_odometry",
                 description="",
             ),
+            # DeclareLaunchArgument("cmd_vel_topic_", description="", default="/lateral"),
             DeclareLaunchArgument(
                 "lidar_odom_frame",
-                default_value="odom_lidar",
+                default_value="ranger/base_link",
                 description="",
             ),
             DeclareLaunchArgument(
                 "wheel_odom_frame",
-                default_value="odom",
+                default_value="ranger/odom",
                 description="",
             ),
             DeclareLaunchArgument(
                 "base_frame",
-                default_value="base_footprint",
+                default_value="ranger/base_footprint",
                 description="",
             ),
             DeclareLaunchArgument(
                 "publish_odom_tf",
-                default_value="true",
+                default_value="false",
                 description="",
                 choices=["true", "false"],
             ),
@@ -49,7 +51,7 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 "visualize",
-                default_value="false",
+                default_value="true",
                 description="",
                 choices=["true", "false"],
             ),
